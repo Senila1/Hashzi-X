6174027d0a61d2d4787dca62f7da70f72d692e31
Senila1 <88504388+Senila1@users.noreply.github.com>
2021-09-22 11:21:36 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,69 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function whatsAsena() {
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [2, 2123, 8]
+
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('Neotro')}${chalk.blue.bold('X')}
+${chalk.white.italic('Neotro-X QR කේතය ')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
+
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('NEUTRO QR Code: '),
+			'NEUTRO;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'NEUTRO;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('90')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Lütfen Bu Kodu Kimseyle Paylaşma ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"EĞER MESAJI KOPYALAMIYORSANIZ LÜTFEN WHATSAPP'I KONTROL EDİN. KENDİ NUMARANIZA QR KODU GÖNDERİLDİ!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
+
+	await conn.connect();
+}
+
+whatsAsena();
